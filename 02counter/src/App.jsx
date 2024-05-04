904a28f863008c9f1f6bc86a51f1d672d00a1c9d
mukul kasana <mukulkasana0001@gmail.com>
2024-05-05 00:01:02 +0530

    place question

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -8,14 +8,28 @@ function App() {
   
   // let counter = 4;
    let  [counter,setcounter]=  useState(0)
+  //  useState forword update to ui or variablesas batches
   let addvalue = () =>{
     console.log(` clicked : ${counter}`)
     // counter = counter +1;
-    setcounter(counter = counter +1)
+    setcounter( counter +1)
+
+    
+    // setcounter( (prevcounter) => prevcounter +1)
+    // it take privious counter
+    // below syntex is same as above prevcounter
+    //   setcounter( prevcounter => prevcounter +1)
+    // we can also write as
+    // setcounter( (counter) => counter +1)
+    // setcounter( counter = counter +1)
+    // **so if we want to edit privious counter ue use as function with argument and also return the value
+
   }
   let removevalue = ()=>{
     console.log(`removed: ${counter}`)
     setcounter(counter - 1)
+    
+   
   }
   return (
     <>
