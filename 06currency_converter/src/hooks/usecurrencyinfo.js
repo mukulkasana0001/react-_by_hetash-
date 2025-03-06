08b7241f06bbf1d9975cfd21641ad38ca4318fd2
mukul kasana <mukulkasana0001@gmail.com>
2025-03-07 00:33:42 +0530

    found api

--- 06currency_converter/src/hooks/usecurrencyinfo.js
+++ 06currency_converter/src/hooks/usecurrencyinfo.js
@@ -8,7 +8,10 @@ function UseCurrencyInfo(currency) {    //  here we create a costum hook ( UseCu
     const [data, setdata] = useState({})
 
     useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+
+       
+        // fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
         ).then((res) => res.json())
             .then((res) => setdata(res[currency]))
         console.log(data)
