a059a4cd44c86249140f3af535f8cce45655fbb7
mukul kasana <mukulkasana0001@gmail.com>
2025-04-05 13:28:02 +0530

    mega project (half complete)

---
+++ 12megProject/src/App.jsx
@@ -0,0 +1,43 @@
+import './App.css';
+import React,{ useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSilce';
+import { Header,Footer } from './components';
+import {Outlet} from 'react-router-dom'
+
+function App() {
+
+  const [loading, setloading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          console.log("Logged in user:", userData);
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setloading(false))
+  }, [])
+
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400' >
+      <div className='w-full block' >
+        <Header/>
+        <main>
+          <Outlet/>
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null
+}
+
+export default App
+
+
---
+++ 12megProject/src/components/index.js
@@ -0,0 +1,35 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import  Login  from "./Login";
+import PostForm from "./post-form/PostForm";
+// import Post from "./Post"
+import PostCard from "./PostCard";
+import AuthLayout from './AuthLayout'
+
+import Button from "./Button";
+import Input from "./Input";
+import Select from './Select';
+
+export {
+    Footer,
+    Header,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+    // Post,
+    PostCard,
+    AuthLayout,
+
+    Button,
+    Input,
+    Select
+}
